76a93ad0c2f8fc7861db442cbf39391d23da6ca4
Joana <jmsi.fernandes@campus.fct.unl.pt>
2023-10-24 02:52:03 +0100

    Added a method to convert an hexadecimal string to a byte array

--- src/Utils.java
+++ src/Utils.java
@@ -32,6 +32,16 @@ public class Utils {
         return bytes;
     }
 
+    public static byte[] hexToByteArray(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) +
+                    Character.digit(hex.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
     public static byte[] generateNonce(){
         //This necessarilly generates a 128-bit nonce. May need to be changed
         byte[] nonce = new byte[16];
